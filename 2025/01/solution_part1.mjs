f54af7aab54ea29dc2add15281904368982c559e
Fernando Cortez <development+github@fernandocortez.dev>
2025-12-02 07:00:46 +0000

    feat: Completed 2025 - Day 1 - Part 1

---
+++ 2025/01/solution_part1.mjs
@@ -0,0 +1,27 @@
+import { open } from 'node:fs/promises';
+
+const file = await open('input.txt');
+
+/** @type {number[]} */
+let instructions = [];
+
+for await (const line of file.readLines()) {
+  if (line.startsWith('L')) {
+      const digits = parseInt(line.substring(1), 10);
+      instructions.push(-1 * digits);
+    } else if (line.startsWith('R')) {
+      const digits = parseInt(line.substring(1), 10);
+      instructions.push(digits);
+    }
+}
+
+let dial = 50;
+let password = 0;
+instructions.forEach((instruction) => {
+  dial = (dial + instruction + 100) % 100;
+  if (dial === 0) {
+    password++;
+  }
+});
+
+console.log('Password:', password);
