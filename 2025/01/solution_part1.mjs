7721cd249199b7bb3018b7b877dd4f13999fdc53
Fernando Cortez <development+github@fernandocortez.dev>
2025-12-02 14:23:26 +0000

    feat: Adding Biome for formatting and linting

--- 2025/01/solution_part1.mjs
+++ 2025/01/solution_part1.mjs
@@ -1,27 +1,27 @@
-import { open } from 'node:fs/promises';
+import { open } from "node:fs/promises";
 
-const file = await open('input.txt');
+const file = await open("input.txt");
 
 /** @type {number[]} */
-let instructions = [];
+const instructions = [];
 
 for await (const line of file.readLines()) {
-  if (line.startsWith('L')) {
-      const digits = parseInt(line.substring(1), 10);
-      instructions.push(-1 * digits);
-    } else if (line.startsWith('R')) {
-      const digits = parseInt(line.substring(1), 10);
-      instructions.push(digits);
-    }
+	if (line.startsWith("L")) {
+		const digits = parseInt(line.substring(1), 10);
+		instructions.push(-1 * digits);
+	} else if (line.startsWith("R")) {
+		const digits = parseInt(line.substring(1), 10);
+		instructions.push(digits);
+	}
 }
 
 let dial = 50;
 let password = 0;
 instructions.forEach((instruction) => {
-  dial = (dial + instruction + 100) % 100;
-  if (dial === 0) {
-    password++;
-  }
+	dial = (dial + instruction + 100) % 100;
+	if (dial === 0) {
+		password++;
+	}
 });
 
-console.log('Password:', password);
+console.log("Password:", password);
